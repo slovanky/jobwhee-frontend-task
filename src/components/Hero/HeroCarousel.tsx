c05ca6cac52517265987dde8f8c88c59e305aafa
Peter Slovanky <slovanky@gmail.com>
2025-04-06 15:41:19 +0200

    updating hero carousel

--- src/components/Hero/HeroCarousel.tsx
+++ src/components/Hero/HeroCarousel.tsx
@@ -14,6 +14,8 @@ import Image from "next/image";
 export default function HeroCarousel() {
   const sliderRef = useRef<SwiperRef>(null);
 
+  const carouselList: string[] = ["/img/hero-slide-1.jpg", "/img/hero-slide-1.jpg", "/img/hero-slide-1.jpg", "/img/hero-slide-1.jpg", "/img/hero-slide-1.jpg"];
+
   return (
     <div className="space-y-8">
       <Swiper
@@ -26,33 +28,15 @@ export default function HeroCarousel() {
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
       >
-        <SwiperSlide>
-          <div className="relative w-full h-[285px] sm:h-[387px] md:h-[596px] lg:h-[715px] bg-black/10 rounded-[18px] sm:rounded-[42px] lg:rounded-[50px] overflow-hidden">
-            <Image src="/img/hero-slide-1.jpg" width={1796} height={716} className="w-full h-full object-cover object-center" alt="" />
-            <HeroBottomCurve className="absolute -bottom-px left-1/2 translate -translate-x-1/2 w-[70%] h-auto text-[#F0F1F4]" />
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="relative w-full h-[285px] sm:h-[387px] md:h-[596px] lg:h-[715px] bg-black/10 rounded-[18px] sm:rounded-[42px] lg:rounded-[50px] overflow-hidden">
-            <Image src="/img/hero-slide-1.jpg" width={1796} height={716} className="w-full h-full object-cover object-center" alt="" />
-            <HeroBottomCurve className="absolute -bottom-px left-1/2 translate -translate-x-1/2 w-[70%] h-auto text-[#F0F1F4]" />
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="relative w-full h-[285px] sm:h-[387px] md:h-[596px] lg:h-[715px] bg-black/10 rounded-[18px] sm:rounded-[42px] lg:rounded-[50px] overflow-hidden">
-            <Image src="/img/hero-slide-1.jpg" width={1796} height={716} className="w-full h-full object-cover object-center" alt="" />
-            <HeroBottomCurve className="absolute -bottom-px left-1/2 translate -translate-x-1/2 w-[70%] h-auto text-[#F0F1F4]" />
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="relative w-full h-[285px] sm:h-[387px] md:h-[596px] lg:h-[715px] bg-black/10 rounded-[18px] sm:rounded-[42px] lg:rounded-[50px] overflow-hidden">
-            <Image src="/img/hero-slide-1.jpg" width={1796} height={716} className="w-full h-full object-cover object-center" alt="" />
-            <HeroBottomCurve className="absolute -bottom-px left-1/2 translate -translate-x-1/2 w-[70%] h-auto text-[#F0F1F4]" />
-          </div>
-        </SwiperSlide>
+        {carouselList &&
+          carouselList.map((slide, index) => (
+            <SwiperSlide key={`hero-carousel-slide-${index}`}>
+              <div className="relative w-full h-[285px] sm:h-[387px] md:h-[596px] lg:h-[715px] bg-black/10 rounded-[18px] sm:rounded-[42px] lg:rounded-[50px] overflow-hidden">
+                <Image src={slide} width={1796} height={716} className="w-full h-full object-cover object-center" alt="" />
+                <HeroBottomCurve className="absolute -bottom-px left-1/2 translate -translate-x-1/2 w-[70%] h-auto text-[#F0F1F4]" />
+              </div>
+            </SwiperSlide>
+          ))}
       </Swiper>
     </div>
   );
